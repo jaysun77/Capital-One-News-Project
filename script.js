105dca11b53a9835a10d4d34d8f5165fa9deab3b
jiesun <jiesun@andrew.cmu.edu>
2020-10-26 02:10:28 -0400

    fix

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-        //const searchForm=document.querySelector('.button');
         const newsList= document.querySelector(".news-list");
-        //searchForm.addEventListener('button',ret)
 
         function ret(e,topi){
           newsList.innerHTML=''
           const apikey='a4ededa4314f4054b67f21aae9def76f'
           let topic = topi;
-          console.log(topi)
           let x=`https://newsapi.org/v2/everything?q=${topic}&apiKey=${apikey}`
           const y = 'http://cors-anywhere.herokuapp.com/';
           const url = `${y}${x}`;
           fetch(url).then((res)=>{
             return res.json()
           }).then((data)=>{
-            //console.log(data)
             data.articles.forEach(article=>{
               let li=document.createElement('li');
               let a=document.createElement('a');
